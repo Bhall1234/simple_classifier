a96f7621df96084de53a4dfc493c459d59673eaf
Bhall1234 <ben.hall17@outlook.com>
2022-05-11 17:53:11 +0100

    glory to CCP

--- simple.py
+++ simple.py
@@ -8,8 +8,12 @@ from PIL import Image
 #image = Image.open("test.png")
 
 
-im = np.array(Image.open('test.jpeg').convert('L')) #you can pass multiple arguments in single line
+im = np.array(Image.open('test.jpeg').convert('L').resize((200,130))) #you can pass multiple arguments in single line
 #print(type(im))
+
+#re_test = np.array(Image.open('test.jpeg').resize((200,130)))
+
+#im_trim = im[128:384, 128:384]
 
 grey_image = Image.fromarray(im).save('test_grey.png')
     
@@ -18,14 +22,19 @@ image = Image.open("test_grey.png")
 data = np.asarray(image)
     #print(type(data))
     #print(data.shape)
-image2 = Image.fromarray(data)
+#image2 = Image.fromarray(data)
     #print(type(image2))
 
     #print (image2)
     #print (image2.size)
+print(f"Data One: {data}")
 
 # Image 2
-im2 = np.array(Image.open('test2.jpeg').convert('L')) #you can pass multiple arguments in single line
+im2 = np.array(Image.open('test2.jpeg').convert('L').resize((200,130))) #you can pass multiple arguments in single line
+
+#re_test2 = np.array(Image.open('test2.jpeg').resize((200,130)))
+
+#im_trim = im[128:384, 128:384]
 
 grey_image2 = Image.fromarray(im2).save('test_grey2.png')
     
@@ -33,13 +42,13 @@ image2 = Image.open("test_grey2.png")
 
 data2 = np.asarray(image2)
 
-image4 = Image.fromarray(data2)
+#image4 = Image.fromarray(data2)
 
-#print(data2)
+print(f"Data Two: {data2}")
 
 def EDM(A,B):
     p1 = np.sum(A**2, axis = 1) [:, np.newaxis]
-    p2 = np.sum(B**2, axis=1) 
+    p2 = np.sum(B**2, axis = 1) 
     p3 = -2 * np.dot(A,B.T)
     #return np.round(np.sqrt(p1+p2+p3),2)
     print(np.round(np.sqrt(p1+p2+p3),2))
